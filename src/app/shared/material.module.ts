9fb03cef53381daaa50a25d2b8bdc85358ed8847
Minhyeong Joe <minhyeong.billy.joe@gmail.com>
2020-07-11 20:40:35 -0700

    implemented Films page UI

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,20 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
 
 // material components
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatPaginatorModule } from "@angular/material/paginator";
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
     MatButtonModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTabsModule,
+    MatPaginatorModule,
   ],
   exports: [
     MatButtonModule,
-    MatDividerModule
-  ]
+    MatDividerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTabsModule,
+    MatPaginatorModule,
+  ],
 })
-export class MaterialModule { }
+export class MaterialModule {}
