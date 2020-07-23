baa95f9269210676af159ea38822c7f03cd920ae
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2020-07-22 17:03:49 -0700

    added film READ operations

--- server/models/film.js
+++ server/models/film.js
@@ -8,7 +8,7 @@ const FilmSchema = mongoose.Schema({
   director: {
     type: [String]
   },
-  releaseDate: {
+  release_date: {
     type: Date
   },
   poster_url: {
@@ -32,6 +32,28 @@ const FilmSchema = mongoose.Schema({
 });
 
 const Film = module.exports = mongoose.model('Film', FilmSchema);
+
+// return films by year
+module.exports.getFilmsByYear = (year, page, lim, callback) => {
+  let start = new Date(year, 0);
+  let end = new Date(year+1, 0);
+  let skipped = (page - 1) * lim;
+  Film.find({release_date: { $gte: start, $lt: end }}).sort({release_date: 'desc'}).skip(skipped).limit(lim).exec(callback);
+}
+
+// return films by title
+module.exports.getFilmsByTitle = (title, page, lim, callback) => {
+  let skipped = (page - 1) * lim;
+  let titleLike = new RegExp(`.*${title}.*`, "gi");
+  Film.find({title: { $regex: titleLike }}).sort({release_date: 'desc'}).skip(skipped).limit(lim).exec(callback);
+}
+
+// return films by director
+module.exports.getFilmsByDirector = (director, page, lim, callback) => {
+  let skipped = (page - 1) * lim;
+  let directorLike = new RegExp(`.*${director}.*`, "gi");
+  Film.find({director: { $regex: directorLike }}).sort({release_date: 'desc'}).skip(skipped).limit(lim).exec(callback);
+}
 
 // return a film by ID
 module.exports.getFilmById = (id, callback) => {
--- server/routes/films.js
+++ server/routes/films.js
@@ -2,13 +2,58 @@ const express = require('express');
 const router = express.Router();
 const Film = require('../models/film');
 
+// @desc  Returns `lim` amount of films at `page` page filtered by `year`
+// @route GET /api/films/byYear?year=:year&page=:page&lim=:lim
+router.get('/byYear', (req, res, next) => {
+  let year = parseInt(req.query.year);
+  let page = parseInt(req.query.page);
+  let lim = parseInt(req.query.lim);
+  Film.getFilmsByYear(year, page, lim, (err, films) => {
+    if (err) {
+      res.json({success: false, message: "Unknown Error occurred"});
+    } else {
+      res.json({success: true, films: films});
+    }
+  });
+});
+
+// @desc  Returns `lim` amount of films at `page` page filtered by `title`
+// @route GET /api/films/byTitle?title=:title&page=:page&lim=:lim
+router.get('/byTitle', (req, res, next) => {
+  let title = req.query.title;
+  let page = parseInt(req.query.page);
+  let lim = parseInt(req.query.lim);
+  Film.getFilmsByTitle(title, page, lim, (err, films) => {
+    if (err) {
+      res.json({success: false, message: "Unknown Error occurred"});
+    } else {
+      res.json({success: true, films: films});
+    }
+  });
+});
+
+// @desc  Returns `lim` amount of films at `page` page filtered by `director`
+// @route GET /api/films/byDirector?director=:director&page=:page&lim=:lim
+router.get('/byDirector', (req, res, next) => {
+  let director = req.query.director;
+  let page = parseInt(req.query.page);
+  let lim = parseInt(req.query.lim);
+  Film.getFilmsByDirector(director, page, lim, (err, films) => {
+    if (err) {
+      res.json({success: false, message: "Unknown Error occurred"});
+    } else {
+      res.json({success: true, films: films});
+    }
+  });
+});
+
 // @desc  Returns a film retrieved by unique ID
 // @route GET /api/films/:id
 router.get('/:id', (req, res, next) => {
   let id = req.params['id'];
   Film.findById(id, (err, film) => {
     if (err) {
-      res.json({success: false, message: err});
+      res.json({success: false, message: "Unknown Error occurred"});
     }
     else {
       res.json({success: true, film: film });
@@ -16,4 +61,4 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
-module.exports = router
+module.exports = router;
