76a0e5a57afd726945cc7ae663fec736c2c94bba
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2020-08-02 17:11:22 -0700

    mobile navbar now closes on link or outside click

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import { APP_TITLE, NAVIGATION } from '../../../environments/constants.json';
 
 @Component({
@@ -10,11 +10,28 @@ export class NavbarComponent implements OnInit {
   public appTitle: string;
   public navigation: {ABOUT: string, FILMS: string, POSTS: string};
 
-  constructor() { }
+  public collapsed : boolean = true;
+
+  constructor(private eRef: ElementRef) { }
 
   ngOnInit(): void {
     this.appTitle = APP_TITLE;
     this.navigation = NAVIGATION;
   }
 
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if(!this.eRef.nativeElement.contains(event.target)) {
+      this.collapseMenu();
+    }
+  }
+
+
+  collapseMenu() {
+    const menu = document.getElementById('navbarNav');
+    const toggler = document.querySelector('.navbar-toggler');
+    menu.classList.remove('show');
+    toggler.attributes['aria-expanded'].value = false;
+    toggler.classList.add('collapsed');
+  }
 }
